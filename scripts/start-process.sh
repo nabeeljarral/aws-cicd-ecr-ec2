5bde5aa603c5394b79146324c7838c03002ef85f
nabeeljarral <40709574+nabeeljarral@users.noreply.github.com>
2024-12-03 17:01:08 +0500

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -6,10 +6,10 @@ sudo -u root aws ecr get-login-password --region ap-south-1 | docker login --use
 sudo -u root docker rm -f `docker ps -q -a`
 sudo -u root docker rmi -f `docker images -q`
 sleep 3
-sudo -u root docker pull 717549417987.dkr.ecr.us-east-2.amazonaws.com/dev/ract:latest
+sudo -u root docker pull 717549417987.dkr.ecr.us-east-2.amazonaws.com/aws-codepipeline-project:latest
 if [[ $? != 0 ]]
 then
     sudo -u root aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 717549417987.dkr.ecr.us-east-2.amazonaws.com
-    sudo -u root docker pull 717549417987.dkr.ecr.us-east-2.amazonaws.com/dev/ract:latest
+    sudo -u root docker pull 717549417987.dkr.ecr.us-east-2.amazonaws.com/aws-codepipeline-project:latest
 fi
-sudo -u root docker run -d -p 80:3000 --name aws-codepipeline-project 717549417987.dkr.ecr.us-east-2.amazonaws.com/dev/ract:latest
+sudo -u root docker run -d -p 80:3000 --name aws-codepipeline-project 717549417987.dkr.ecr.us-east-2.amazonaws.com/aws-codepipeline-project:latest
